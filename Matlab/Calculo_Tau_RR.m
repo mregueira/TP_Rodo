2bea55b7fe09804f14518dfeff382966bac8720b
mregueira <mregueira@itba.edu.ar>
2020-10-29 15:28:15 -0300

    edit matriz

--- Matlab/Calculo_Tau_RR.m
+++ Matlab/Calculo_Tau_RR.m
@@ -1,4 +1,6 @@
-  %% RR
+%% RR
+clear all;
+clc;
 syms th1 th2 thd1 thd2  thdd1 thdd2 real
 Q=[th1 th2];
 Qd=[thd1 thd2];
@@ -16,6 +18,13 @@ i=1;
 Link1=Link('alpha',alpha(i),'a',a(i),'d',d(i),'modified');
 i=2;
 Link2=RevoluteMDH('alpha',alpha(i),'a',a(i),'d',d(i));
+% Masa concentrada
+Ixx1 = 0;
+Iyy1 = 0;
+Izz1 = 0;
+Ixx2 = 0;
+Iyy2 = 0;
+Izz2 = 0;
 
 I1=[Ixx1 0 0 ; 0 Iyy1 0 ; 0 0 Izz1];
 I2=[Ixx2 0 0 ; 0 Iyy2 0 ; 0 0 Izz2];
@@ -28,10 +37,10 @@ Link2.m=m2;
 
 Tool = transl([L2, 0, 0]); % Offset de la herramienta (End Effector)
 
-Robot= SerialLink( [Link1 Link2], 'tool', Tool,'name', 'Chaplin' );
+Robot = SerialLink( [Link1 Link2], 'tool', Tool,'name', 'Chaplin' );
 Robot.gravity=gravedad;
-T1 = Link1.A(th1) 
-T2 = Link2.A(th2)
+T1 = Link1.A(th1) ;
+T2 = Link2.A(th2);
 Tt = double(T1) * double(T2) * Tool;
 
 q0 = [0 0];
@@ -40,9 +49,9 @@ M=Robot.inertia(Q);
 V=Robot.coriolis(Q,Qd)*Qd'; %Matriz de coriolis
 G=Robot.gravload(Q);
 Tau=M*Qdd'+V+G;
-% 
+
 % Taui=Robot.itorque(Q,Qdd);
-TAU = Robot.rne(Q, Qd, Qdd)
+TAU = Robot.rne(Q, Qd, Qdd);
 TAU(1)
 TAU(2)
 
